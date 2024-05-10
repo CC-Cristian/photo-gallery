b694431adfe960a1e54a8827f0aaeadf638a5ee4
CC-Cristian <crielprog@gmail.com>
2024-05-10 16:24:03 -0500

    Final
    Sin errores

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { PhotoService } from '../services/photo.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public photos: any[] = [];
+  constructor(public photoService: PhotoService, public alertController: AlertController) {}
 
-  constructor() {}
-
+  addPhotoToGallery(webPath: string, description: string) {
+    this.photoService.addNewToGallery();
+    this.photos.unshift({
+      webPath: webPath,
+      description: description // Aquí agregamos la descripción asociada a la foto
+    });
+  }
 }
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift(capturedPhoto);
+  }
+}
